5f5c6176278d7d42349e2598646c2927fc944f73
babu0312 <pankaj2victor@gmail.com>
2015-05-17 17:04:57 +0530

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "matrix" object, 
+## which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## setInvMatrix the value of the Inverse of Matrix
+## getInvMatrix the value of the Inverse of Matrix
 
-## Write a short comment describing this function
+## The second function, cacheSolve computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix. If the inverse has already been 
+## calculated (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
+
+## Note - Matrix given as input to makecacheMatrix and set should be 
+## a Square Matrix.
+
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      InvMatrix <- NULL
+      set <- function(SqMatrix) {
+            x <<- SqMatrix
+            InvMatrix <<- NULL
+      }
+      get <- function() x
+      
+      setInvMatrix <- function(InverseMatrix) InvMatrix <<- InverseMatrix
+      
+      getInvMatrix <- function() InvMatrix
+      
+      list(set = set, get = get,
+           setInvMatrix = setInvMatrix,
+           getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. If already computed then
+## retrieves it from cache, otherwise calculates it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      InvMatrix <- x$getInvMatrix()
+      
+      if(!is.null(InvMatrix)){
+            message("getting cached data...")
+            return(InvMatrix)
+      }
+      
+      SqMatrix <- x$get()
+      InvMatrix <- solve(SqMatrix, ...)
+      x$setInvMatrix(InvMatrix)
+      InvMatrix
 }
